89a59a9f9a0f9bca3b03dbf0d80ff318699050e3
jackiewelte <jwwelte@ucdavis.edu>
2026-01-02 02:32:05 -0800

    debugging

--- root/src/javascript/bookwebapp.js
+++ root/src/javascript/bookwebapp.js
@@ -873,9 +873,9 @@ function joinLeaveGroup(checkbox, groups, groupKey, groupMembers, user, userName
         }
         // alert('Joined: ' + groupName + ' at ' + groups[groupKey]);
 
-        console.log(groupMembers[groupKey])
+        console.log(groupMembers)
         groupMembers[groupKey].push(user)
-        console.log(groupMembers[groupKey])
+        console.log(groupMembers)
         // users.setAttribute('data-users', groupMembers[groupKey])
         // console.log(JSON.parse(users.dataset.users))
 
@@ -891,9 +891,9 @@ function joinLeaveGroup(checkbox, groups, groupKey, groupMembers, user, userName
 
             for (let i = 0; i < Object.keys(groupMembers[groupKey]).length; i++) {
                 if (groupMembers[groupKey][i].username === userName) {
-                    console.log(groupMembers[groupKey])
+                    console.log(groupMembers)
                     delete groupMembers[groupKey][i]
-                    console.log(groupMembers[groupKey])
+                    console.log(groupMembers)
                     break
                 }
             }
