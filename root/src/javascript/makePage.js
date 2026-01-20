8507bafeb0ff3fd309d55142c40fdfd4c325edc9
jackiewelte <jwwelte@ucdavis.edu>
2026-01-19 18:49:05 -0800

    Update stylesheet header in makePage.js content variable

--- root/src/javascript/makePage.js
+++ root/src/javascript/makePage.js
@@ -9,7 +9,7 @@ export function makePage() {
     };
     var content = "<!DOCTYPE html><html lang=\"en\" dir=\"ltr\">" +
                     "<head><meta charset=\"utf-8\" name=\"viewport\" content=\"width=device-width\" initial-scale=\"1\"></meta><title>Book Webapp</title>" +
-                    "<link rel=\"stylesheet\" type=\"text/css\" href=\"../src/css/style.css\">" +
+                    "<link rel=\"stylesheet\" type=\"text/css\" href=\"../src/css/main.css\">" +
                     "<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">" +
                     "<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>" +
                     "<link href=\"https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap\" rel=\"stylesheet\">" +
