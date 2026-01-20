17e4dac5a284e8a0577b89c95987d1ca4511fac9
jackiewelte <jwwelte@ucdavis.edu>
2026-01-19 19:20:30 -0800

    Update font of discover page

--- root/src/javascript/main.js
+++ root/src/javascript/main.js
@@ -1860,14 +1860,9 @@ document.addEventListener("DOMContentLoaded", function() {
                         '#88b439'
                         // '#53a989'
                     ],
-                    borderColor: [
-                        // '#262d38'
-                        '#70896b'
-                        // 'rgba(255, 99, 132, 1)',
-                        // 'rgba(54, 162, 235, 1)',
-                        // 'rgba(255, 206, 86, 1)',
-                        // 'rgba(75, 192, 192, 1)',
-                    ],
+                    // borderColor: [
+                    //     '#70896b'
+                    // ],
                     borderWidth: 4
                 }],
                 labels: ['<300', '300-499', '500+']
